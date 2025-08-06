e2f416b6bef1f3c9cfb46532ea371f558dac01a7
Xabibullayev murodillo <xabibullayevmurodillo2906@gmail.com>
2025-08-06 13:49:46 +0500

    store done

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('common/', include('common.urls')),
+    path('api/', include('accounts.urls')),
+    path('api/', include('common.urls')),
+    path('api/', include('store.urls')),
 ]
